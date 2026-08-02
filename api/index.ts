dc3a1d7d945f5c1dd92ce63e95f26e2ce47cc5c5
Alan Felix <felixnascc@gmail.com>
2026-08-02 15:02:16 -0300

    Refactor server structure for Vercel

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
-import { animeRoutes, apiRoutes, firebaseRoutes } from './routes';
+import { animeRoutes, apiRoutes, firebaseRoutes } from '../server/routes';
 
 const app = express();
 
